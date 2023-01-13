45d042b0c41e8bb56f516b639b0388ae57cc1e0c
Berry <105915738+RatArt@users.noreply.github.com>
2023-01-13 20:25:53 +0100

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ db_login = {
 
     "db_host": "uvdb65.active24.cz",
     "db_username": "berryratsnake",
-    "db_password": "6N6HVlmvSI",
+    "db_password": "psw",
     "db_name": "berryratsnake"
 
 }
